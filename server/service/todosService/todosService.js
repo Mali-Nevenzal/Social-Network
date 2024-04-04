eed7f2375e64f6318bb0c5d46680301e6da8749d
Sari Safra <s0504175747@gmail.com>
2024-04-04 20:44:41 +0300

    ca

--- server/service/todosService/todosService.js
+++ server/service/todosService/todosService.js
@@ -18,7 +18,7 @@ export class TodosService {
 
     async addTodo(todoItem) {
         const queryTodos = addQuery("todos","user_id,title,completed","?,?,?");
-        console.log("mali"+todoItem)
+        console.log("mali "+todoItem)
         const result =  await query(queryTodos, [todoItem.user_id,todoItem.title,todoItem.completed]);
         console.log("add to do resualt" +result);
         return result;
