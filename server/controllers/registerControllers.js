3f2f24ca7dfb2aa9e84f6cd5c7b28a1341aaac76
Sari Safra <s0504175747@gmail.com>
2024-04-07 17:00:01 +0300

    varfication

--- server/controllers/registerControllers.js
+++ server/controllers/registerControllers.js
@@ -25,7 +25,7 @@ export class RegisterController {
             const startIndex = (req.query.page_ - 1) * req.query.limit_;
             const sort = req.query.sort_ || "passwordd";
            //inter if there is a same email.
-            const resultItem = await registerService.getRegister(req.body.username,req.query.limit_,startIndex,sort);                 
+            const resultItem = await registerService.getRegister(req.body.email,req.query.limit_,startIndex,sort);                 
             let algorithm = "sha256"                
             let key = req.body.password;
             let digest2 = crypto.createHash(algorithm).update(key).digest("base64") 
@@ -48,7 +48,7 @@ export class RegisterController {
             const registerService = new RegisterService();
             const startIndex = (req.query.page_ - 1) * req.query.limit_;
             const sort = req.query.sort_ || "password";
-            const resultItem = await registerService.getRegister(req.body.username,req.query.limit_,startIndex,sort);
+            const resultItem = await registerService.getRegister(req.body.email,req.query.limit_,startIndex,sort);
             if(resultItem[0])
             {
                 let algorithm = "sha256";
@@ -80,8 +80,8 @@ export class RegisterController {
         try {
             console.log("register");
             const registerService = new RegisterService();
-            await registerService.deleteRegister(req.query.username);
-            return res.status(200).json({ status: 200, data: req.query.uaername });
+            await registerService.deleteRegister(req.query.email);
+            return res.status(200).json({ status: 200, data: req.query.email });
         }
         catch (ex) {
             const err = {}
@@ -93,9 +93,25 @@ export class RegisterController {
 
     async updateRegister(req, res,next) {
         try {
-                        //אימות
-            //במקרה של עדכון קוד, נדרוש קוד ישן קוד חדש נבצע אימות , ונאפשר במידה שהאימות תקין
             const registerService = new RegisterService();
+         const startIndex = (req.query.page_ - 1) * req.query.limit_;
+            const sort = req.query.sort_ || "password";
+            const resultItem = await registerService.getRegister(req.body.email,req.query.limit_,startIndex,sort);
+            if(resultItem[0])
+            {
+                let algorithm = "sha256";
+                let key = req.body.prevPassword;
+                let digest= crypto.createHash(algorithm).update(key).digest("base64"); 
+                if(resultItem[0].password!==digest)
+                {
+                    res.status(500).json({ status: 500});
+                }
+
+            }
+            else{
+                res.status(500).json({ status: 500});
+            }
+                        //במקרה של עדכון קוד, נדרוש קוד ישן קוד חדש נבצע אימות , ונאפשר במידה שהאימות תקין
             await registerService.updateRegister(req.body);
             res.status(200).json({ status: 200, data: req.params.email });
         }
