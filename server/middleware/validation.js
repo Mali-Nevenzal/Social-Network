b6ede4c2c600100ae4a602137cc22e54a236eb64
Sari Safra <s0504175747@gmail.com>
2024-04-07 22:09:41 +0300

    ס

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -2,6 +2,7 @@ function emailValid(email)
 {
     var emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     if (email !== '' && email.match(emailFormat)) { return true; }
+    console.log("the email is not validL: "+email)
     return false;
 }
 
@@ -9,6 +10,7 @@ function phonenumberValid(inputtxt) {
     var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
     if(inputtxt.match(phoneno)) 
       return true;
+      console.log("the phone is not validL: "+inputtxt)
     return false;
   }
 
@@ -16,6 +18,7 @@ function phonenumberValid(inputtxt) {
     var regex = /^[a-zA-Z]+$/;
     if ((input.length > 11) || !regex.test(input)) 
       return false;
+      console.log("the userName is not validL: "+input)
     return true;
   }
 
